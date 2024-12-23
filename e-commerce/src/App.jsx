8f433e14db8c38aab0652201514f09175d6cc6a2
Enzo <enzobutoric@gmail.com>
2024-12-23 10:49:25 -0300

    v1.3

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -2,19 +2,16 @@ import React from 'react'
 import NavBar from './components/NavBar'
 import ItemListContainer from './components/ItemListContainer'
 import { BrowserRouter, Routes, Route } from "react-router";
+import ItemDetailContainer from './components/ItemDetailContainer';
 
 const App = () => {
   return (
-    // <>
-    //   <div>
-    //     <NavBar />
-    //     <ItemListContainer bg='gray' colorTexto='white' />
-    //   </div>
-    // </>
     <BrowserRouter>
      <NavBar />
      <Routes>
-       <Route path="/home" element={<ItemListContainer />} />
+       <Route path="/" element={<ItemListContainer />} />
+       <Route path="/category/:id" element={<ItemListContainer />} />
+       <Route path="/item/:id" element={<ItemDetailContainer />} />
      </Routes>
    </BrowserRouter>
     
---
+++ e-commerce/src/components/ItemDetailContainer.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+
+function ItemDetailContainer () {
+    const [item, setItem] = useState()
+
+    useEffect(() => {
+        
+    }, [])
+    return (
+        <div>Item Detail</div>
+    )
+}
+
+export default ItemDetailContainer
